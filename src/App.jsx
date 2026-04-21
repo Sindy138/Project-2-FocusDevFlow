1398a8e53b66d1a5a8005aa93611faece97a6ac5
Sindy Davila <sindydl@gmail.com>
2026-04-21 17:15:34 +0100

    integration of pages in router app

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Timer from "./components/Timer";
-import TimerControls from "./components/TimerControls";
-import ProjectTaskManager from "./components/ProjectTaskManager";
-import { useFlowTimer } from "./hooks/useFlowTimer";
+import Header from "./components/layout/Header";
+import HomePage from "./pages/HomePage";
+import WrapUpPage from "./pages/WrapUpPage";
+import ProjectPage from "./pages/ProjectPage";
 import "./App.css";
-import DailyLog from "./components/DailyLog";
-
-const Home = () => {
-  return (
-    <div className="home-layout">
-      <Timer />
-
-      <ProjectTaskManager />
-
-      <div className="control-section">
-        <TimerControls />
-        <DailyLog />
-      </div>
-    </div>
-  );
-};
-
-const WrapUp = () => <div>Página Daily Wrap-up</div>;
-const Projects = () => <div>Página de Proyectos</div>;
 
 function App() {
   return (
@@ -33,9 +13,9 @@ function App() {
 
         <main className="main-content">
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/wrap-up" element={<WrapUp />} />
-            <Route path="/projects" element={<Projects />} />
+            <Route path="/" element={<HomePage />} />
+            <Route path="/wrap-up" element={<WrapUpPage />} />
+            <Route path="/projects" element={<ProjectPage />} />
           </Routes>
         </main>
       </div>
